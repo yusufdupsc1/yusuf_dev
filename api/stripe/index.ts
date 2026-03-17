b58f912d2265396f56eb9cfbce63496c4db0fc62
morpheus <morpheus@mx>
2026-03-17 16:53:23 +0600

    Fix Stripe API version

--- api/stripe/index.ts
+++ api/stripe/index.ts
@@ -2,7 +2,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2025-02-24.acacia',
   typescript: true,
 });
 
